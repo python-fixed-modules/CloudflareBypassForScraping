c95e2aa5ad49fd44ec8d40b66550a82ea9cbec62
VoidPro <voidpro.dev@gmail.com>
2024-10-07 09:43:22 +0900

    Update server.py

--- server.py
+++ server.py
@@ -210,11 +210,12 @@ async def v1(payload: RequestModel):
         driver = bypass_cloudflare(payload.url, 5, log, timeout=payload.maxTimeout, proxy=payload.proxy)
         html = driver.html
         cookies_json = driver.cookies(as_dict=True)
+        user_agent = str(driver.user_agent)
         try:
             driver.quit()
         except:
             pass
-        return ResponseModel(status="ok", solution={"cookies": [{"name": a, "value":b} for a,b in cookies_json.items()], "kv_cookies": {a:b for a,b in cookies_json.items()}, "userAgent": driver.user_agent, "html": html})
+        return ResponseModel(status="ok", solution={"cookies": [{"name": a, "value":b} for a,b in cookies_json.items()], "kv_cookies": {a:b for a,b in cookies_json.items()}, "userAgent": user_agent, "html": html})
     except Exception as e:
         return ResponseModel(status="failed", message=str(e))
 
